4cf88f6093e79e300c95e8473f3f8ccb147cac38
Nicolas Martinez <elnico-2001@hotmail.com>
2023-12-28 14:31:11 -0300

    Improving SEO

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -4,16 +4,16 @@ import '../styles/Navigator.css'
 export function Navigator() {
   return(
     <nav className='navigator'>
-      <Link className='navigator-link' to='about' smooth={true} offset={0} duration={500}>
+      <Link href='about' className='navigator-link' to='about' smooth={true} offset={0} duration={500}>
         ¿Quien soy?
       </Link>
-      <Link className='navigator-link' to='skills' smooth={true} offset={0} duration={500}>
+      <Link href='skills' className='navigator-link' to='skills' smooth={true} offset={0} duration={500}>
         Habilidades
       </Link>
-      <Link className='navigator-link' to='projects' smooth={true} offset={0} duration={500}>
+      <Link href='projects' className='navigator-link' to='projects' smooth={true} offset={0} duration={500}>
         Proyectos
       </Link>
-      <Link className='navigator-link' to='talk' smooth={true} offset={0} duration={500}>
+      <Link href='talk' className='navigator-link' to='talk' smooth={true} offset={0} duration={500}>
         Hablemos
       </Link>
     </nav>
