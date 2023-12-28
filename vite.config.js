a222699e0901e8e5dd6b3095290ace29eb183080
Nicolas Martinez <elnico-2001@hotmail.com>
2023-12-28 02:08:07 -0300

    Add domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://elnico91.github.io/portfolio/'
+  base: 'https://nicolasmartinez.cl/'
 })
